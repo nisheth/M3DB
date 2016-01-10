efbd947ffae09c73991f33e1c2c692471a79ecec
Shaun Norris <norrissw@vcu.edu>
2016-01-09 21:44:34 -0500

    but of course they have to be in order

--- Django/M3DB/models.py
+++ Django/M3DB/models.py
@@ -7,26 +7,24 @@ class UploadedFiles(models.Model):
     forward_read = models.CharField(max_length=100)
     reverse_read = models.CharField(max_length=100)
 
-class AbundanceProfile(models.Model):
-    exp_id = models.ForeignKey(Experiment)
-    sample_id = models.ForeignKey(Sample)
-    analysis = models.ForeignKey('Analysis', null=True, blank=True)
-    taxonomy = models.ForeignKey('Taxonomy', null=True, blank=True)
-    num_reads = models.IntegerField(null=True, blank=True)
-    abundance = models.FloatField(null=True, blank=True)
-    score = models.FloatField(null=True, blank=True)
-    status = models.CharField(max_length=2, blank=True)
-    miscellaneous = models.CharField(max_length=1000, blank=True)
-    taxonomy_level = models.CharField(max_length=1000, blank=True)
-    taxonomy_name = models.CharField(max_length=1000, blank=True)
-
-class Analysis(models.Model):
-    analysis_id = models.IntegerField(primary_key=True)
+class Metadata(models.Model):
+    sample_id = models.CharField(max_length=256)
     name = models.CharField(max_length=256)
-    description = models.CharField(max_length=1000)
-    parameters = models.CharField(max_length=256)
-    comments = models.CharField(max_length=500)
+    value = models.CharField(max_length=256)
+
+
+class RefDb(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=500)
+    url = models.CharField(max_length=1000)
+    version = models.CharField(max_length=256)
+
+class Taxonomy(models.Model):
+    tax_name = models.TextField()
+    tax_level = models.TextField()
     refdb = models.ForeignKey(RefDb)
+    parent_id = models.IntegerField(null=True, blank=True)
+    external_id = models.CharField(max_length=20)
 
 class Project(models.Model):
     name = models.CharField(max_length=1000, unique=True, blank=True)
@@ -48,18 +46,6 @@ class Experiment(models.Model):
     def __unicode__(self):
         return self.name
 
-class Metadata(models.Model):
-    sample_id = models.CharField(max_length=256)
-    name = models.CharField(max_length=256)
-    value = models.CharField(max_length=256)
-
-
-class RefDb(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.CharField(max_length=500)
-    url = models.CharField(max_length=1000)
-    version = models.CharField(max_length=256)
-
 class Sample(models.Model):
     name = models.CharField(max_length=1000, blank=True)
     index1 = models.CharField(max_length=20,blank=True)
@@ -68,7 +54,29 @@ class Sample(models.Model):
     def __unicode__(self):
         return self.name
 
+class Analysis(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1000)
+    parameters = models.CharField(max_length=256)
+    comments = models.CharField(max_length=500)
+    refdb = models.ForeignKey(RefDb)
+
+class AbundanceProfile(models.Model):
+    exp_id = models.ForeignKey(Experiment)
+    sample_id = models.ForeignKey(Sample)
+    analysis = models.ForeignKey(Analysis)
+    taxonomy = models.ForeignKey(Taxonomy)
+    num_reads = models.IntegerField(null=True, blank=True)
+    abundance = models.FloatField(null=True, blank=True)
+    score = models.FloatField(null=True, blank=True)
+    status = models.CharField(max_length=2, blank=True)
+    miscellaneous = models.CharField(max_length=1000, blank=True)
+    taxonomy_level = models.CharField(max_length=1000, blank=True)
+    taxonomy_name = models.CharField(max_length=1000, blank=True)
+
 class SampleStatistics(models.Model):
+    exp_id = models.ForeignKey(Experiment)
+    sample_id = models.ForeignKey(Sample)
     total_reads = models.IntegerField(null=True, blank=True)
     overlapping_reads = models.IntegerField(null=True, blank=True)
     per_overlapping_reads = models.FloatField(null=True, blank=True)
@@ -84,16 +92,6 @@ class SampleStatistics(models.Model):
     hq_avg_length = models.FloatField(null=True, blank=True)
     hq_avg_quality = models.FloatField(null=True, blank=True)
     hq_avg_mee = models.FloatField(null=True, blank=True)
-    exp_id = models.ForeignKey(Experiment)
-    sample_id = models.ForeignKey(Sample)
-
-class Taxonomy(models.Model):
-#    tax_id = models.IntegerField(primary_key=True)
-    tax_name = models.TextField()
-    tax_level = models.TextField()
-    refdb = models.ForeignKey(RefDb)
-    parent_id = models.IntegerField(null=True, blank=True)
-    external_id = models.CharField(max_length=20)
 
 class ReadAssignment(models.Model):
     exp_id = models.ForeignKey(Experiment)
