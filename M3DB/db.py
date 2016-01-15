d3a8dff6f8a7c36f786466c95963f4c8e2f94ba6
Shaun Norris <norrissw@vcu.edu>
2016-01-14 22:41:45 -0500

    first pass at updating to match django models changes. I a feature to make this use django objects may be worthwhile

--- M3DB/db.py
+++ M3DB/db.py
@@ -24,22 +24,21 @@ def __init__(config):
 def createproject(config,args):
     # Create a new project #
     _db_connection,_db_cur,_hs_connection,_hs_cur = __init__(config)
-    _db_cur.execute("INSERT INTO project VALUES (default, '%s','%s','%s','%s','%s','%s')" % (args.name,args.pi,args.email,args.desc,args.user,args.user))
+    _db_cur.execute("INSERT INTO "M3DB_project" VALUES (default, '%s','%s','%s','%s','%s','%s')" % (args.name,args.pi,args.email,args.desc,args.user,args.user))
     _db_connection.commit()
     __del__(_db_connection,_hs_connection)
 def getprojectid(config,expname):
     # This may not be necessary or may need a different method #
     _db_connection,_db_cur,_hs_connection,_hs_cur = __init__(config)
-   # _db_cur.execute("SELECT project_id FROM project WHERE name='%s'" % args.project)
-    _db_cur.execute("SELECT a.project_id FROM project a INNER JOIN experiment b ON a.project_id = b.project_id WHERE b.exp_name = '%s'" % expname)
+    _db_cur.execute("SELECT a.id FROM "M3DB_project" a INNER JOIN "M3DB_experiment" b ON a.id = b.project_id_id WHERE b.exp_name = '%s'" % expname)
     return _db_cur.fetchone()
     __del__(_db_connection,_hs_connection)
 def createexp(config,args):
     # Create a new experiment #
     _db_connection,_db_cur,_hs_connection,_hs_cur = __init__(config)
-    _db_cur.execute("SELECT project_id FROM project WHERE name = '%s'" % args.project)
+    _db_cur.execute("SELECT id FROM "M3DB_project" WHERE name = '%s'" % args.project)
     projectid = int(re.sub('\W','',str(_db_cur.fetchone())))
-    _db_cur.execute("INSERT INTO experiment VALUES(default,%i,'%s','%s','%s','%s','%s')" % (projectid,args.name,args.date,args.plat,args.region,args.user))
+    _db_cur.execute("INSERT INTO "M3DB_experiment" VALUES(default,'%s','%s','%s','%s','%s',%i)" % (args.name,args.date,args.plat,args.region,args.user,projectid))
     _db_connection.commit()
     __del__(_db_connection,_hs_connection)
 def insertfile(config,args):
@@ -48,7 +47,6 @@ def insertfile(config,args):
     table = args.table
     filename = open(args.filename)
     _db_cur.copy_from(filename,table)
-    #_db_cur.execute("COPY %s FROM '%s' WITH DELIMITER E'\t';" % (table,filename))
     filename.close()
     __del__(_db_connection,_hs_connection)
 def query(config,query):
@@ -66,7 +64,7 @@ def gettax(config,dbver):
     taxdict = defaultdict()
     _db_connection,_db_cur,_hs_connection,_hs_cur = __init__(config)
     dict_cur = _db_connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    dict_cur.execute("select tax_name,tax_id from taxonomy where refdb_id = %i" % dbver)
+    dict_cur.execute("select tax_name,id from "M3DB_taxonomy" where refdb_id = %i" % dbver)
     for row in dict_cur.fetchall():
         taxdict[row[0]] = row[1]
     __del__(_db_connection,_hs_connection)
@@ -89,14 +87,14 @@ def getfasta(config,sampleid,fastaname):
         print "Found FASTA file already...\n This is okay if you already ran an analysis on this sample... Otherwise please remove this file and re-run the script\n Continuing..."
 def getaid(config,value):
     _db_connection,_db_cur,_hs_connection,_hs_cur = __init__(config)
-    _db_cur.execute("SELECT analysis_id FROM analysis WHERE parameters = 'Cutoff: %0.1f';" % float(value))
+    _db_cur.execute("SELECT id FROM "M3DB_analysis" WHERE parameters = 'Cutoff: %0.1f';" % float(value))
     analysisid = re.sub("\'|\(|\)|\,","",str(_db_cur.fetchone()))
     if analysisid == None:
         desc = "The RDP Classifier is a naive Bayesian classifier which was developed to provide rapid taxonomic placement based on rRNA sequence data. The RDP Classifier can rapidly and accurately classify bacterial and archaeal 16s rRNA sequences, and Fungal LSU sequences. It provides taxonomic assignments from domain to genus, with confidence estimates for each assignment. The RDP Classifier likely can be adapted to additional phylogenetically coherent bacterial taxonomies."
         tool = "RDP Classifier"
-        _db_cur.execute("INSERT INTO analysis (name,description,parameters,comments,refdb_id) SELECT '%s','%s','Cutoff: %0.1f','%s',%i WHERE NOT EXISTS (select parameters from analysis where parameters = 'Cutoff: %f');" % (tool,desc,float(thresh),"Added through m3dbcli",1,float(thresh)))
+        _db_cur.execute("INSERT INTO "M3DB_analysis" (name,description,parameters,comments,refdb_id) SELECT '%s','%s','Cutoff: %0.1f','%s',%i WHERE NOT EXISTS (select parameters from M3DB_analysis where parameters = 'Cutoff: %f');" % (tool,desc,float(thresh),"Added through m3dbcli",1,float(thresh)))
         _db_connection.commit()
-        _db_cur.execute("SELECT analysis_id FROM analysis WHERE parameters = 'Cutoff: %0.1f';" % float(thresh))
+        _db_cur.execute("SELECT analysis_id FROM "M3DB_analysis" WHERE parameters = 'Cutoff: %0.1f';" % float(thresh))
         analysisid = re.sub("\'|\(|\)|\,","",str(_db_cur.fetchone()))
     __del__(_db_connection,_hs_connection)
     return analysisid
@@ -108,7 +106,7 @@ def getexpid(config,expname):
     _db_connection,_db_cur,_hs_connection,_hs_cur = __init__(config)
     # Do not insert if the sample already exists (covered by the WHERE NOT EXISTS...)
     try:
-        _db_cur.execute("SELECT exp_id FROM experiment WHERE name = '%s';" % expname)
+        _db_cur.execute("SELECT id FROM "M3DB_experiment" WHERE name = '%s';" % expname)
         expid = int(re.sub('\W','',str(_db_cur.fetchone())))
     except:
         print "Failed to get Experiment ID, Check Experiment ID and try again"
@@ -117,7 +115,7 @@ def getexpid(config,expname):
 def getsampleid(config,sname,expid):
     _db_connection,_db_cur,_hs_connection,_hs_cur = __init__(config)
     # Do not insert if the sample already exists (covered by the WHERE NOT EXISTS...)
-    _db_cur.execute("SELECT sample_id FROM sample WHERE name = '%s' and exp_id = %i;" % (sname,expid))
+    _db_cur.execute("SELECT id FROM "M3DB_sample" WHERE name = '%s' and exp_id_id = %i;" % (sname,expid))
     sampleid = int(re.sub('\W','',str(_db_cur.fetchone())))
     __del__(_db_connection,_hs_connection)
     return sampleid
@@ -150,7 +148,7 @@ def insertsampstat(config,filename,exp,samplename,fastq):
     line.pop(0) #remove the samplename
     line.insert(0,expid)
     try:
-        _db_cur.execute("INSERT INTO sample (exp_id,name,index1,index2) SELECT %i,'%s','%s','%s' WHERE NOT EXISTS (SELECT name from sample where name='%s' and exp_id = %i);" % (expid,samplename,index1,index2,samplename,expid))
+        _db_cur.execute("INSERT INTO "M3DB_sample" (name,index1,index2,exp_id_id) SELECT '%s','%s','%s',%i WHERE NOT EXISTS (SELECT name from "M3DB_sample" where name='%s' and exp_id_id = %i);" % (samplename,index1,index2,expid,samplename,expid))
         _db_connection.commit()
     except:
         _db_connection.rollback()
@@ -163,7 +161,7 @@ def insertsampstat(config,filename,exp,samplename,fastq):
         o.write(line)
     try: # Insert the data
     	filename = open(filename)
-    	_db_cur.copy_expert("COPY sample_statistics FROM STDIN with DELIMITER E'\t' CSV HEADER;",filename)
+    	_db_cur.copy_expert("COPY "M3DB_samplestatistics" FROM STDIN with DELIMITER E'\t' CSV HEADER;",filename)
     	_db_connection.commit()
     	filename.close()
     #	__del__(_db_connection,_hs_connection)
@@ -183,13 +181,13 @@ def insertabundance(config,abundanceprof,flag):
     print "Inserting Abundance Profile..."
     if flag:
         abundanceprof = open(abundanceprof)
-        _db_cur.copy_from(abundanceprof,'abundance_profile',sep='\t',columns=('sample_id','taxonomy_level','taxonomy_name','num_reads','abundance','score','analysis_id','taxonomy_id','exp_id','status','miscellaneous'))
+        _db_cur.copy_from(abundanceprof,'"M3DB_abundanceprofile"',sep='\t',columns=('sample_id_id','taxonomy_level','taxonomy_name','num_reads','abundance','score','analysis_id_id','taxonomy_id_id','exp_id_id','status','miscellaneous'))
         #_db_cur.execute("COPY abundance_profile(sample_name,taxonomy_level,taxonomy_name,num_reads,abundance,avg,analysis_id,taxonomy_id,experiment_name,status,miscellaneous,sample_id) FROM '%s' with DELIMITER E'\t' CSV HEADER;" % abundanceprof)
         _db_connection.commit()
         abundanceprof.close()
     else:
         abundanceprof = open(abundanceprof)
-        _db_cur.copy_from(abundanceprof,'abundance_profile',sep='\t',columns=('exp_id','sample_id','analysis_id','taxonomy_id','num_reads','abundance','score','status','miscellaneous','taxonomy_level','taxonomy_name'))
+        _db_cur.copy_from(abundanceprof,'"M3DB_abundanceprofile"',sep='\t',columns=('exp_id_id','sample_id_id','analysis_id_id','taxonomy_id_id','num_reads','abundance','score','status','miscellaneous','taxonomy_level','taxonomy_name'))
         #_db_cur.execute("COPY abundance_profile(experiment_name,sample_name,analysis_id,taxonomy_id,num_reads,abundance,avg,status,miscellaneous,taxonomy_level,taxonomy_name,sample_id) FROM '%s' with DELIMITER E'\t';" % abundanceprof)
         _db_connection.commit()
         abundanceprof.close()
